91a2da0792442230610837a2722654116973b4ed
Koray <91742967+Koraytopuz@users.noreply.github.com>
2023-05-12 19:25:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, total = 0;
+
+        System.out.print("Bir sayı giriniz:");
+        n = input.nextInt();
+
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                total = total + i;
+            }
+        }
+        if (n == total) {
+
+            System.out.println(n + " Mükemmel sayıdır.");
+        } else {
+            System.out.println(n +  " Mükemmel sayı değildir.");
+
+        }
+
+    }
+}
